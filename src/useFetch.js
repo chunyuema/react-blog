6b250fb25f390171a0b9b8a695eab4ab441015b0
Chunyue Ma <cma01@wesleyan.edu>
2021-01-15 10:20:08 -0500

    changed a tag to link tag & abort controller

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,14 +1,17 @@
 // This file is defined to be used to fetch data from a certain url endpoint
 import {useState, useEffect} from 'react';
+
 const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
-    // fire some functions after every randering
+
+    // fire some functions after every rendering
     // [] will contain the dependencies wheb userEffect will run
     useEffect(() => {
+        const abortCont = new AbortController();
         setTimeout(()=> {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     if (!res.ok) {throw Error('Could Not Get The Blog Data');}
                     return res.json();
@@ -19,10 +22,16 @@ const useFetch = (url) => {
                     setError(null);
                 })
                 .catch(err => {
-                    setError(err.message);
-                    setIsPending(false);
+                    if (err.name === 'AbortError') {
+                        console.log('Fetch Aborted');
+                    } else {
+                        setError(err.message);
+                        setIsPending(false);
+                    }
                 })
         }, 1000);
+        // using abort controller
+        return () => abortCont.abort();
     }, [url]); 
 
     return {data, isPending, error}; 
