1dedc995de7dd906e9e10b395ca2616208f342e4
Chunyue Ma <cma01@wesleyan.edu>
2021-01-19 14:27:58 -0500

    link to blogs by custom hook reuse & create blog form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Switch>
             <Route exact path='/'><Home /></Route>
             <Route path='/create'><Create  /></Route>
+            <Route path='/blogs/:id'><BlogDetails  /></Route>
           </Switch>
         </div>
       </div>
